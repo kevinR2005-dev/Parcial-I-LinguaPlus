b16471996e9a1187ed935a037eea4929ef20e7ec
nicosanchez29 <nicolas.sanchezc@uqvirtual.edu.co>
2026-09-25 17:16:51 -0500

    Se corrige un error dentro de la estructura de las clases y se agrega codigo en clase estudiante y docente

--- src/modelo/Estuadiante.java
+++ src/modelo/Estuadiante.java
@@ -1,4 +1,51 @@
 package modelo;
 
+import java.time.LocalDate;
+
 public class Estuadiante {
+
+    private final String documentoIdentidad;
+    private final String nombreCompleto;
+    private final String telefono;
+    private final String correoElectronico;
+    private final int edad;
+    private final LocalDate fechaRegistro;
+
+    public Estuadiante(String documentoIdentidad, String nombreCompleto, String telefono, String correoElectronico, int edad, LocalDate fechaRegistro) {
+        this.documentoIdentidad = documentoIdentidad;
+        this.nombreCompleto = nombreCompleto;
+        this.telefono = telefono;
+        this.correoElectronico = correoElectronico;
+        this.edad = edad;
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public String getDocumentoIdentidad() {
+        return documentoIdentidad;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    @Override
+    public String toString() {
+        return nombreCompleto + "(" + documentoIdentidad + ")";
+    }
 }
