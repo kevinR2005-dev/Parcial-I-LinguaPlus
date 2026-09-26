93f50b8aa7ba6fd3c71cdd86ff382840f8558a5a
robikev <kevin.robim@uqvirtual.edu.co>
2026-09-25 20:09:18 -0500

    Se agrega PeriodoRepositorio, ProgramaRepositorio y ServicioAdicionalRepositorio

--- src/repositorio/PeriodoRepositorio.java
+++ src/repositorio/PeriodoRepositorio.java
@@ -1,4 +1,34 @@
 package repositorio;
 
-public class PeriodoRepositorio {
+import java.util.*;
+import java.util.function.Function;
+
+
+public class PeriodoRepositorio<T, ID> implements Repositorio<T, ID> {
+    private final Map<ID, T> datos = new LinkedHashMap<>();
+    private final Function<T, ID> extractorId;
+
+    public PeriodoRepositorio(Function<T, ID> extractorId) {
+        this.extractorId = extractorId;
+    }
+
+    @Override
+    public void guardar(T entidad) {
+        datos.put(extractorId.apply(entidad), entidad);
+    }
+
+    @Override
+    public Optional<T> buscarPorId(ID id) {
+        return Optional.ofNullable(datos.get(id));
+    }
+
+    @Override
+    public List<T> listarTodos() {
+        return new ArrayList<>(datos.values());
+    }
+
+    @Override
+    public void eliminar(ID id) {
+        datos.remove(id);
+    }
 }
